f855de5b5955694c25ce6400d7bf4b3479aad022
sidaoui <sidaouimanel@gmail.com>
2024-11-29 22:34:37 +0100

    first commit

---
+++ src/main/java/com/mycompany/socproject/Socproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.socproject;
+
+/**
+ *
+ * @author user
+ */
+public class Socproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
